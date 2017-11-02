6a891725af3d2aa4062475600d85427c9937d8fe
Stacey Tay <staceytay@users.noreply.github.com>
2017-11-02 19:03:57 +0800

    Make "C-x o" prompt for a target window when there are more than 2

--- init.el
+++ init.el
@@ -75,10 +75,17 @@
   :config
   (exec-path-from-shell-initialize))
 
-(use-package scratch)
 
 (use-package rg
   :bind ("M-s" . rg))
+
+(use-package scratch)
+
+(use-package switch-window
+  :bind ("C-x o" . switch-window)
+  :config
+  (setq-default switch-window-shortcut-style 'alphabet)
+  (setq-default switch-window-timeout nil))
 
 (use-package which-key
   :diminish which-key-mode
