bd237cc9b9fe1d752d482cf285519997378ad85d
Stacey Tay <staceytay@users.noreply.github.com>
2017-11-07 20:01:17 +0800

    Install ibuffer

--- init.el
+++ init.el
@@ -91,6 +91,30 @@
   :config
   (exec-path-from-shell-initialize))
 
+(use-package ibuffer
+  :bind ("C-x C-b" . ibuffer)
+  :config
+  (use-package ibuffer-vc
+    :config (add-hook 'ibuffer-hook 'ibuffer-vc-set-filter-groups-by-vc-root))
+  (require 'ibuffer)
+  (require 'ibuffer-vc)
+  (define-ibuffer-column size-h
+    ;; Use human readable Size column instead of original one
+    (:name "Size" :inline t)
+    (cond
+     ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+     ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+     (t (format "%8d" (buffer-size)))))
+  (setq ibuffer-formats
+  	'((mark modified read-only vc-status-mini " "
+  		(name 18 18 :left :elide)
+  		" "
+  		(size-h 9 -1 :right)
+  		" "
+  		(mode 16 16 :left :elide)
+  		" "
+  		filename-and-process)))
+  (setq ibuffer-filter-group-name-face 'font-lock-doc-face))
 
 (use-package rg
   :bind ("C-c s" . rg))
